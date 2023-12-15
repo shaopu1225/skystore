c2f2a93ad607688da14580597c08e22b5dc64d97
SongShaopu1998 <shaopu@berkeley.edu>
2023-12-14 18:17:28 -0800

    prepare for testing

--- store-server/experiment/host.py
+++ store-server/experiment/host.py
@@ -57,23 +57,29 @@ def create_instance(
         assert tup[1].strip() == "", f"Command failed, err: {tup[1]}"
 
     # validate arguments
-    aws_region_list = ["us-west-2"]
-    init_regions_list = ['aws:eu-south-2', 
-                         'aws:eu-west-1', 
-                         'aws:ca-central-1', 
-                         'aws:eu-west-3', 
-                         'aws:ap-southeast-2', 
-                         'aws:eu-south-1', 
-                         'aws:eu-central-1', 
-                         'aws:ap-southeast-1', 
-                         'aws:ap-northeast-1', 
-                         'aws:eu-central-2', 
-                         'aws:ap-northeast-3', 
-                         'aws:ap-southeast-3', 
-                         'aws:sa-east-1', 
-                         'aws:us-west-1', 
-                         'aws:ap-east-1', 
-                         'aws:ap-northeast-2']
+    aws_region_list = ["eu-west-2"]
+    init_regions_list = ['ap-northeast-2', 
+                         'us-west-1', 
+                         'eu-central-2', 
+                         'ap-southeast-3', 
+                         'eu-north-1', 
+                         'eu-south-1', 
+                         'ap-south-1', 
+                         'me-central-1', 
+                         'eu-central-1', 
+                         'sa-east-1', 
+                         'ap-southeast-2', 
+                         'ca-central-1', 
+                         'us-east-1', 
+                         'us-west-2', 
+                         'eu-south-2', 
+                         'ap-northeast-1', 
+                         'ap-east-1', 
+                         'me-south-1', 
+                         'ap-southeast-1', 
+                         'ap-northeast-3', 
+                         'eu-west-3', 
+                         'eu-west-1']
     skystore_bucket_prefix = "skystore"
 
     # validate AWS regions
